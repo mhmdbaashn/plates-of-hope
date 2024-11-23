83ba6fbf2a2e4e8d7fd394d4f0b4c21e404069af
Mohammed Baashn <ir.mnmn.maob@gmail.com>
2024-11-23 20:07:43 +0000

    Create project and app files

--- my_project/asgi.py
+++ my_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for fight_hunger project.
+ASGI config for my_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fight_hunger.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_project.settings')
 
 application = get_asgi_application()
